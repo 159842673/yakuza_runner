7d4153c39a75acdc9577022cbfdbb0ac7e08f35c
Hugo <cellier.hugo@gmail.com>
2022-01-17 17:11:53 +0100

    Shield commit

--- Assets/scrpits/shield.cs
+++ Assets/scrpits/shield.cs
@@ -6,19 +6,16 @@ using UnityEngine.InputSystem;
 
 public class shield : MonoBehaviour
 {
-    public GameObject Shield;
+    public GameObject shields;
 
-    public shield(GameObject shield)
-    {
-        this.Shield = shield;
-    }
+   
 
     private bool activateShield;
     // Start is called before the first frame update
     void Start()
     {
         activateShield = false;
-        shield.SetActive(false);
+        shields.SetActive(false);
     }
 
     private static void SetActive(bool v)
@@ -33,12 +30,12 @@ public class shield : MonoBehaviour
        
             if (ctx.performed)
             {
-                shield.SetActive(true);
+                shields.SetActive(true);
                 ActiveShield = true;
             }
             if(ctx.canceled)
             {
-                shield.SetActive(false);
+                shields.SetActive(false);
                 ActiveShield = true;
             }
        
